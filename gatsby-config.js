7a6f8c5c4a5b954bc0125fef087619e0e7e8e5c2
Alex Dudley <alexdudman@gmail.com>
2020-01-27 23:03:37 +0000

    Given config a proper name relating to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby x Tailwind x Framer Motion`,
-    description: `An opinionated starter for Gatsby v2 with TailwindCSS, PostCSS and Framer Motion page transitions.`,
+    title: `Alex James`,
+    description: `WEB DESIGNER // FRONT-END DEVELOPER NOTTINGHAM`,
     author: `@samuelgoddard`,
   },
   plugins: [
