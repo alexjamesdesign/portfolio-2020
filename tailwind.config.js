a0c5de677d93af7baf15a0be2056d5a33cba0385
Alex Dudley <alexdudman@gmail.com>
2020-01-27 21:46:44 +0000

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          'DIN'
+        ],
+      },
+    }
+  },
+  plugins: [
+    require('tailwindcss-transition')({
+      standard: 'all .3s ease',
+      transitions: {
+        'slow': 'all 0.7s ease',
+      }  
+    })
+  ],
+  corePlugins: {
+    container: false
+  }
+}
