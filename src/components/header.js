6e0baf36cba27408c14c3dad6cc9d9dfd7676948
Alex Dudley <alexdudman@gmail.com>
2020-02-04 12:11:35 +0000

    Added in the nav again and tidied some of the index

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { motion } from 'framer-motion'
+import Nav from '../components/nav'
+
+import SvgLogo from "../components/logo"
 
 const Header = ({ siteTitle }) => (
   <header className="w.full bg-black">
     
-    <div className="relative h-64 flex items-center">
-      <div className="block w-2/5 text-white flex items-center justify-end"><p>LOGO</p></div>
-      <div className="block w-3/5">
-        <h1 className="text-5xl p-5 uppercase tracking-widest bg-white text-black m-0 max-w-xl">{siteTitle}</h1>
-        <p className="text-left p-5 text-white bg-gray-700 w-full">WEB DESIGNER <br /> // FRONT-END DEVELOPER <span>NOTTINGHAM</span></p>
-      </div>
-    </div>
-
-    <div className="bg-yellow-500 h-12"></div>
+    <Nav />
 
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,34 +29,17 @@ const IndexPage = ({data: { page }}) => {
       
       <SEO title="Home" />
 
-      <motion.section
-        variants={container}
-        initial="hidden" 
-        animate="visible"
-        className="container"
-      >
-        <motion.div 
-          className="content"
-          variants={item}
-          transition="easeInOut"
-        >
+      <motion.section variants={container} initial="hidden"  animate="visible" className="container" >
+        <motion.div className="content" variants={item} transition="easeInOut" >
           {JSON.stringify(page)}
           <Hero heading={page.heroTitle} />
         </motion.div>
 
-        <motion.div 
-          className="content"
-          variants={item}
-          transition="easeInOut"
-        >
+        <motion.div className="content" variants={item} transition="easeInOut" >
           <hr className="block my-8" />
         </motion.div>
 
-        <motion.div 
-          className="content"
-          variants={item}
-          transition="easeInOut"
-        >
+        <motion.div className="content" variants={item} transition="easeInOut" >
           <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           <h2>Lorem ipsum dolor sit amet</h2>
