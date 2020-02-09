f46be15a9ede478912526d2951ee895ce78b7d63
Alex Dudley <alexdudman@gmail.com>
2020-02-09 22:19:01 +0000

    More styling tweaks for mobile hero

--- src/components/skill.js
+++ src/components/skill.js
@@ -11,9 +11,9 @@ const Skill = ({ title, content, image, order, icon, contentPadding }) => (
             <Img fluid={image.fluid} className="border-white border-8 w-full" alt={image.alt} key={image.title} />
         </div>
         <div className="w-full tablet:w-2/3 z-20">
-          <div className="w-full flex items-center justify-center tablet:justify-left flex flex-wrap tablet:flex-no-wrap tablet:flex-no-wrap tablet:-ml-20 skill-order-odd">
-            <img src={icon.url} className="bg-white p-3 w-32 h-32 w-full tablet:w-auto" alt={icon.alt} key={icon.title}/>
-            <span className="uppercase text-black tracking-widest font-bold text-3xl mb-0 pl-2">{title}</span>
+          <div className="w-full flex items-center flex flex-wrap tablet:flex-no-wrap tablet:flex-no-wrap tablet:-ml-20 skill-order-odd -mt-16 tablet:-mt-1">
+            <img src={icon.url} className="bg-white p-3 w-32 h-32" alt={icon.alt} key={icon.title}/>
+            <span className="uppercase text-black tracking-widest font-bold text-3xl mb-0 pl-2 w-full tablet:w-auto text-center tablet:text-left">{title}</span>
           </div>
             <p className={`${contentPadding} text-center tablet:text-left`}>{content}</p>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -48,19 +48,6 @@ const IndexPage = ({data: { page }}) => {
         </motion.div>
       </motion.section>
 
-      <motion.section variants={container} initial="hidden"  animate="visible" className="container" >
-        <motion.div className="content" variants={item} transition="easeInOut" >
-          <hr className="block my-8" />
-        </motion.div>
-
-        <motion.div className="content" variants={item} transition="easeInOut" >
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-          <h2>Lorem ipsum dolor sit amet</h2>
-          
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </motion.div>
-      </motion.section>
     </>
   )
 }
