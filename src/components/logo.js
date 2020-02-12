9b88cbfb9535137d29b0d887b74a295091692208
Alex Dudley <alexdudman@gmail.com>
2020-02-12 21:57:33 +0000

    Changed width param of svg logo to try and fig a bug on mobile

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SvgLogo = props => (
-  <svg width="auto" height="auto" viewBox="0 0 360 307" fill="none" {...props}>
+  <svg width="100%" height="auto" viewBox="0 0 360 307" fill="none" {...props}>
     <path
       d="M127.873 0L0 285.011h46.425l81.448-187.752c19.276 45.387 59.294 138.023 65.159 152.231 7.33 17.761 22.805 57.51 83.891 57.51 70.86 0 83.077-55.818 83.077-79.499V0h-41.538v227.501c0 16.915-12.218 35.521-39.91 37.213-22.154 1.353-38.281-10.149-45.611-27.064L127.873 0z"
       fill="#fff"
