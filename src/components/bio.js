c33dbd33f863805bb3174f46dd637937bc3954c3
Alex James <hello@alexjamesdesign.co.uk>
2020-12-23 23:24:02 +0000

    Progress with bio. Started on timeline bar

--- src/components/bio.js
+++ src/components/bio.js
@@ -10,11 +10,11 @@ const Bio = ({ title, content, image }) => (
         
         <div className="container tablet:flex">
 
-          <div className="w-full tablet:w-1/3">
-            <Img fluid={image.fluid} className="border-white border-8 w-full" alt={image.alt} key={image.title} />
+          <div className="w-full tablet:w-1/6">
+            <span className="timeline-bar h-full block w-12 bg-red-800 rounded-3xl"></span>
           </div> 
 
-          <div className="w-full tablet:w-2/3">
+          <div className="w-full tablet:w-5/6 tablet:pl-8">
 
             <h1 className="uppercase">{title}</h1>
             <div className="text-white" dangerouslySetInnerHTML={{ __html: content }} />
