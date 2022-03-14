8ba7d1ca156612408c5a28fdaa5702ef0db2b3f0
Alex James <alexjames@Alexs-MacBook-Pro.local>
2022-03-14 20:19:29 +0000

    Added favicon and updated theme color and backgruond in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,10 +25,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#03152D`,
+        theme_color: `#03152D`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/fav-icon.png`,
       },
     },
     {
